c325fdf41222769117b58bf770813e6f7c55b8dd
AnryChe <stroyspecialist@rambler.ru>
2019-12-22 16:04:06 +0200

    finished creating foreign keys.

--- Course project/Prteam.sql
+++ Course project/Prteam.sql
@@ -1,105 +1,153 @@
 DROP DATABASE IF EXISTS prteam;
 CREATE DATABASE prteam;
 USE prteam;
+
 DROP TABLE IF EXISTS users;
-CREATE TABLE Users (
-	id INT UNSIGNED,
-	name varchar(255),
-	contacts varchar(255),
-	prfl_id INT UNSIGNED
-);
+CREATE TABLE users (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	FirstName VARCHAR(20) NOT NULL,
+    LastName VARCHAR(20) NOT NULL,
+    Phone VARCHAR(20) NOT NULL UNIQUE
+	);
+
+DROP TABLE IF EXISTS photos;
+CREATE TABLE photos (
+  id int UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+  filename VARCHAR(10) NOT NULL
+  );
+ 
+DROP TABLE IF EXISTS profiles;
+CREATE TABLE profiles (
+  user_id int NOT NULL,
+  sex char(1) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  birthday date DEFAULT NULL,
+  hometown varchar(100) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
+  photo_id int(10) UNSIGNED DEFAULT NULL,
+  CONSTRAINT profiles__users_fk
+ 	 FOREIGN KEY (user_id)  REFERENCES users (id) ON DELETE CASCADE,
+  CONSTRAINT profiles_photos_fk
+ 	 FOREIGN KEY (photo_id)  REFERENCES photos (id) ON DELETE SET NULL  
+    );
 
 DROP TABLE IF EXISTS owner_statuses;
 CREATE TABLE owner_statuses (
-	id int(10) unsigned NOT NULL,
+	id int PRIMARY KEY AUTO_INCREMENT,
 	name varchar(10),
 	describes varchar(255)
 	);
 
 DROP TABLE IF EXISTS skills;
 CREATE TABLE skills (
-	id int(10) unsigned NOT NULL,
+	id int PRIMARY KEY AUTO_INCREMENT,
 	name varchar(10),
 	describes varchar(255)
 	);
 
 DROP TABLE IF EXISTS Users_skills;
 CREATE TABLE Users_skills (
-	users_id int(10) unsigned NOT NULL,
-	skill_id int(10) unsigned NOT NULL,
+	user_id int(10)  NOT NULL,
+	skill_id int(10)  NOT NULL,
 	start_data timestamp NULL,
-	finish_data timestamp NULL
+	finish_data timestamp NULL,
+	CONSTRAINT user_skills_users_fk
+		FOREIGN KEY (user_id)  REFERENCES users (id) ON DELETE CASCADE,  
+	CONSTRAINT user_skills_skills_fk
+   	 FOREIGN KEY (skill_id)  REFERENCES skills (id) ON DELETE RESTRICT
 	); 
 
 DROP TABLE IF EXISTS projects;
 CREATE TABLE projects (
-	id int(10) unsigned NOT NULL,
+	id int PRIMARY KEY AUTO_INCREMENT,
 	name varchar(10),
 	describes varchar(255),
-	owner_id int(10) unsigned NOT NULL
+	owner_id int NOT NULL,
+	CONSTRAINT projects_owners_fk
+		FOREIGN KEY (owner_id)  REFERENCES users (id) ON DELETE RESTRICT 
 	);
- 
-DROP TABLE IF EXISTS projects_tasks;
-CREATE TABLE projects_tasks (
-	prjct_id int(10) unsigned NOT NULL,
-	tsks_id int(10) unsigned NOT NULL,
-	users_id int(10) unsigned NOT NULL,
-	start_data timestamp NULL,
-	finish_data timestamp NULL,
-	deadline timestamp NULL
-	);
-	
+ 	
 DROP TABLE IF EXISTS tasks;
 CREATE TABLE tasks (
-	id int(10) unsigned NOT NULL,
+	id int(10) PRIMARY KEY AUTO_INCREMENT,
 	name varchar(10),
 	describes varchar(255)
 	);
 
+DROP TABLE IF EXISTS projects_tasks;
+CREATE TABLE projects_tasks (
+	prjct_id int(10)  NOT NULL,
+	tsk_id int(10)  NOT NULL,
+	user_id int(10)  NOT NULL,
+	start_data timestamp NULL,
+	finish_data timestamp NULL,
+	deadline timestamp NULL,
+	CONSTRAINT projects_tasks_projects_fk
+		FOREIGN KEY (prjct_id)  REFERENCES projects (id) ON DELETE RESTRICT,
+	CONSTRAINT projects_tasks_tasks_fk
+		FOREIGN KEY (tsk_id)  REFERENCES tasks (id) ON DELETE RESTRICT,
+	CONSTRAINT projects_tasks_users_fk
+		FOREIGN KEY (user_id)  REFERENCES users (id) ON DELETE RESTRICT 
+	);
+
 DROP TABLE IF EXISTS tasks_requests;
 CREATE TABLE tasks_requests (
-	id int(10) unsigned NOT NULL,
-	tsks_id int(10) unsigned NOT NULL,
-	skill_id int(10) unsigned NOT NULL
+	id int(10) PRIMARY KEY AUTO_INCREMENT,
+	tsk_id int(10)  NOT NULL,
+	skill_id int(10)  NOT NULL,
+	CONSTRAINT tasks_requests_tasks_fk
+		FOREIGN KEY (tsk_id)  REFERENCES projects (id) ON DELETE RESTRICT,
+	CONSTRAINT tasks_requests_skills_fk
+		FOREIGN KEY (skill_id)  REFERENCES skills (id) ON DELETE RESTRICT	
 	);
 
 DROP TABLE IF EXISTS teams;
 CREATE TABLE teams (
-	id int(10) unsigned NOT NULL,
-	prjct_id int(10) unsigned NOT NULL,
-	users_id int(10) unsigned NOT NULL
+	id int(10)  PRIMARY KEY AUTO_INCREMENT,
+	prjct_id int(10)  NOT NULL,
+	user_id int(10)  NOT NULL,
+	owner_id int(10)  NOT NULL,
+	CONSTRAINT teams_project_fk
+		FOREIGN KEY (prjct_id)  REFERENCES projects (id) ON DELETE CASCADE,
+	CONSTRAINT teams_users_fk
+		FOREIGN KEY (user_id)  REFERENCES users (id) ON DELETE CASCADE,
+	CONSTRAINT teams_owner_fk
+		FOREIGN KEY (owner_id)  REFERENCES owner_statuses (id) ON DELETE RESTRICT ON UPDATE CASCADE
 	);
 
-CREATE TABLE contracts (
-	owner_id INT UNSIGNED NOT NULL,
-	employee_id INT UNSIGNED NOT NULL,
-	status_id INT UNSIGNED NOT NULL,
-	requested_at  DATETIME DEFAULT NOW(),
-	confirmed_at  DATETIME
-	);
-	
- 
-CREATE TABLE Contract_statuses (
+DROP TABLE IF EXISTS contract_statuses;
+CREATE TABLE contract_statuses (
 	id INT  UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	name VARCHAR(10) NOT NULL UNIQUE
 );
 
+DROP TABLE IF EXISTS contracts;
+CREATE TABLE contracts (
+	owner_id INT  NOT NULL,
+	employee_id INT  NOT NULL,
+	status_id INT UNSIGNED NOT NULL,
+	requested_at  DATETIME DEFAULT NOW(),
+	confirmed_at  DATETIME,
+	CONSTRAINT contracts_owner_fk
+		FOREIGN KEY (owner_id)  REFERENCES owner_statuses (id) ON DELETE RESTRICT,
+	CONSTRAINT contracts_users_fk
+		FOREIGN KEY (employee_id)  REFERENCES users (id) ON DELETE RESTRICT,
+	CONSTRAINT contracts_contract_statuses_fk
+		FOREIGN KEY (status_id)  REFERENCES contract_statuses (id) ON DELETE RESTRICT ON UPDATE CASCADE
+	);
+
+DROP TABLE IF EXISTS messages;
 CREATE TABLE messages (
   id int(10) unsigned NOT NULL AUTO_INCREMENT,
-  from_user_id int(10) unsigned NOT NULL,
-  to_user_id int(10) unsigned NOT NULL,
+  from_user_id int(10)  NOT NULL,
+  to_user_id int(10)  NOT NULL,
   body text COLLATE utf8_unicode_ci NOT NULL,
   is_important tinyint(1) DEFAULT NULL,
   is_delivered tinyint(1) DEFAULT NULL,
   created_at datetime DEFAULT current_timestamp(),
-  PRIMARY KEY (id)
+  PRIMARY KEY (id),
+  CONSTRAINT messages_from_users_fk
+		FOREIGN KEY (from_user_id)  REFERENCES users (id) ON DELETE CASCADE,
+	CONSTRAINT messages_to_users_fk
+		FOREIGN KEY (to_user_id)  REFERENCES users (id) ON DELETE CASCADE
 );
 
-CREATE TABLE profiles (
-  user_id int(10) unsigned NOT NULL,
-  sex char(1) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-  birthday date DEFAULT NULL,
-  hometown varchar(100) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
-  photo_id int(10) unsigned DEFAULT NULL,
-  PRIMARY KEY (user_id)
-  );
+
