f6ed2c0fa3fdc4860fcfe0bff4f36821fec458b1
AnryChe <stroyspecialist@rambler.ru>
2020-01-05 10:00:08 +0200

    little changes with "drop" before "create view"

--- Course project/Prteam_views.sql
+++ Course project/Prteam_views.sql
@@ -1,12 +1,12 @@
 USE prteam;
 
 -- для разминки упорядочим пользователей по порядку
-
+DROP VIEW IF EXISTS own_stat;
 CREATE VIEW own_stat AS SELECT * FROM users_statuses ORDER BY user_id, owner_statuses_id;
-
 SELECT * FROM own_stat;
 
 -- Представление, показывающее задачи проектов в порядке их старта
+DROP VIEW IF EXISTS ordered_tasks;
 CREATE VIEW ordered_tasks AS SELECT projects.name AS Project, tasks.name AS Task, projects_tasks.start_data FROM projects
 	LEFT JOIN projects_tasks ON (projects.id = projects_tasks.prjct_id)
 	LEFT JOIN tasks ON (projects_tasks.tsk_id = tasks.id)
